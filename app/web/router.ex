ee7f00bd237e6f4902b73b32cffbd6b607911c89
Edgar Pino <edgarbeltran@me.com>
2016-10-14 23:47:18 -0500

    Created more controllers

--- app/web/router.ex
+++ app/web/router.ex
@@ -18,6 +18,8 @@ defmodule App.Router do
 
     get "/", PageController, :index
     get "/hello", HelloController, :index
+    get "/hello/:messenger", HelloController, :show
+
   end
 
   # Other scopes may use custom stacks.
--- app/web/views/page_view.ex
+++ app/web/views/page_view.ex
@@ -1,3 +1,8 @@
 defmodule App.PageView do
   use App.Web, :view
+
+  def subtitle do
+    "Hello World!"
+  end
+
 end
